cb7c5dc1c6ae7125dd0f97f695fdfd86423f2625
KarloFab <fabijanickarlo1@gmail.com>
2019-08-08 22:51:16 +0200

    Created simple blog posts app

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Posts from './Posts/Posts';
+import './Blog.css';
+import { Route } from 'react-router-dom';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Route path="/" exact component = {Posts} />
+            </div>
+        );
+    }
+}
+
+export default Blog;
