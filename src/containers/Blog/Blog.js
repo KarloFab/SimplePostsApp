511e4797bc4f2be96800b4d439124d376b21e108
KarloFab <fabijanickarlo1@gmail.com>
2019-08-12 22:59:22 +0200

    Added routing for showing full post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import { Route, NavLink } from 'react-router-dom';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -22,6 +23,7 @@ class Blog extends Component {
                 </header>
                 <Route path="/" exact component = {Posts} />
                 <Route path="/new-post" component = {NewPost} />
+                <Route path="/:id" exact component = {FullPost} />
             </div>
         );
     }
